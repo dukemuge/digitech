f41268abcd8b09590e3310c14c1167f7b5245880
dukemuge <dukemuge232@gmail.com>
2022-07-28 18:02:32 +0300

    add project setup

---
+++ src/main/java/com/challenge/digitech/DigitechApplication.java
@@ -0,0 +1,13 @@
+package com.challenge.digitech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigitechApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DigitechApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/challenge/digitech/DigitechApplicationTests.java
@@ -0,0 +1,13 @@
+package com.challenge.digitech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DigitechApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
